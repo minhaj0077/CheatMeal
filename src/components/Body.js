8e8608e630df4c561b14859544a13fd918a07261
Minhaj <minhajdudekula007@gmail.com>
2025-01-07 23:26:37 +0530

    used useStare hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,13 @@
 import Restrocard from "./Restrocard";
 import resList from "../utils/resList";
+import { useState } from "react";
 
 
 
 const Body = () => {
+
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+
     return (
         <div className="body">
 
@@ -12,11 +16,16 @@ const Body = () => {
                 <div className="search">
                     search & button
                 </div>
-                <div className="location-filter">locations</div>
+                <div className="filter">
+                    <button className="rating-filter" onClick={() => { const filteredList = listOfRestaurants.filter((res) => res.info.rating.aggregate_rating > 4);
+                        setListOfRestaurants(filteredList);
+                    }} >Top Reted Restaurants</button>
+
+                </div>
             </div>
 
             <div className="cards-container">
-                {resList.map((restaurant) => ( <Restrocard key={restaurant.info.resId} resData= {restaurant} /> ))}
+                {listOfRestaurants.map((restaurant) => ( <Restrocard key={restaurant.info.resId} resData= {restaurant} /> ))}
             </div>
 
         </div>
